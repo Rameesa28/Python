b61552f77d338d1f54517f66d807691560c9a96f
Rameesa28 <146942818+Rameesa28@users.noreply.github.com>
2023-12-12 17:35:42 +0530

    Add files via upload

---
+++ python/csvfile.py
@@ -0,0 +1,7 @@
+import csv
+with open('employee.csv','r')as file1:
+   reader=csv.reader(file1)
+   for row in reader:
+      print(row)
+          
+          
---
+++ python/string.py
@@ -0,0 +1,4 @@
+s=input("enter a word:")
+c=s[0]#
+str=s.replace(s[0],'$')#
+c+str1[1:]
