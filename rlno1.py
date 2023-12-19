6f3db0747f284fd69186c1baca561421d1fb2782
Rameesa28 <146942818+Rameesa28@users.noreply.github.com>
2023-12-19 17:35:18 +0530

    Add files via upload

---
+++ rlno1.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self,rlno,name):
+        self.rlno=rlno
+        self.name=name
+stud1=Student(10,"Rameesa")
+print("roll no :",stud1.rlno)
+print("name:",std1.name)
